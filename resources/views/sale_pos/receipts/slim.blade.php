a41971fd879465f3416731e1e2c7f24fce0e880b
sahmtech <145831839+leengithub@users.noreply.github.com>
2025-12-21 18:54:58 +0300

    Fix( print invoice ):
    - make the paragraph label is printed from the DB.

--- resources/views/sale_pos/receipts/slim.blade.php
+++ resources/views/sale_pos/receipts/slim.blade.php
@@ -645,22 +645,19 @@
                                 </td>
                             </tr> --}}
         @if (!empty($receipt_details->line_taxes))
-            <div class="flex-box">
-                <p class="width-50 text-right">@lang('lang_v1.line_taxes'):</p>
-                <p class="width-50 text-right">
-                    @if (!empty($receipt_details->line_taxes))
-                        @foreach ($receipt_details->line_taxes as $k => $v)
-                            <strong><small>{{ $k }}</small></strong> - <span
-                                class="display_currency pull-right"
-                                data-currency_symbol="true">{{ $v }}</span><br>
-                        @endforeach
-                    @else
-                        0.00
-                    @endif
-                </p>
-            </div>
+            @foreach ($receipt_details->line_taxes as $k => $v)
+                <div class="flex-box">
+                    <p class="width-50 text-right">
+                        <strong><small>{{ $k }}</small></strong>:
+                    </p>
+                    <p class="width-50 text-right">
+                        <span class="display_currency pull-right" data-currency_symbol="true">
+                            {{ $v }}
+                        </span>
+                    </p>
+                </div>
+            @endforeach
         @endif
-
         @if ($receipt_details->round_off_amount > 0)
             <div class="flex-box">
                 <p class="width-50 text-right">
