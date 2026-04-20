b35c918a538c311b1b6234600fdbdc0018030cc4
MohammadAmen <mohammadamen042@gmail.com>
2026-04-20 14:03:11 +0300

    ..

--- app/Utils/TransactionUtil.php
+++ app/Utils/TransactionUtil.php
@@ -1765,7 +1765,7 @@ class TransactionUtil extends Util
             $qr_code_details[] = $business_details->tax_number_1;
         }
 
-       $qr_code_text = implode("\n", $qr_code_details);
+       $qr_code_text = implode(" ", $qr_code_details);
     }
 
     if ($transaction->status == 'final') {
