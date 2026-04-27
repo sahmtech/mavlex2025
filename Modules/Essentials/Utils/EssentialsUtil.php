5afd82f76afb411416802044259deaa3fdbb3789
Ali <alielkeway2020@gmail.com>
2026-04-28 00:57:52 +0300

    "att_1"

--- Modules/Essentials/Utils/EssentialsUtil.php
+++ Modules/Essentials/Utils/EssentialsUtil.php
@@ -645,7 +645,7 @@ class EssentialsUtil extends Util
                 1 => 'January', 2 => 'February', 3 => 'March', 4 => 'April',
                 5 => 'May', 6 => 'June', 7 => 'July', 8 => 'August',
                 9 => 'September', 10 => 'October', 11 => 'November', 12 => 'December',
-            ][$monthNum] ?? $monthStart->format('F'),
+            ][$month] ?? $monthStart->format('F'),
             'days_before' => $daysBefore,
             'days' => $days,
             'days_after' => $daysAfter,
